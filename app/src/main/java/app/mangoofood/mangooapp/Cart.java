89b29f4958a054f0e54a51df857159c8de67d2a4
akshyK <45514003+akshyK@users.noreply.github.com>
2018-12-10 23:10:41 +0530

    21st client side done

--- app/src/main/java/app/mangoofood/mangooapp/Cart.java
+++ app/src/main/java/app/mangoofood/mangooapp/Cart.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -80,10 +82,13 @@ public class Cart extends AppCompatActivity {
         alertDialog.setTitle("One more Step");
         alertDialog.setMessage("Enter your address : ");
 
-        final EditText edtAddress = new EditText(Cart.this);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        edtAddress.setLayoutParams(lp);
-        alertDialog.setView(edtAddress);
+        LayoutInflater inflater=this.getLayoutInflater();
+        View order_address_comment=inflater.inflate(R.layout.order_address_comment,null);
+
+        final MaterialEditText edtAddress = (MaterialEditText)order_address_comment.findViewById(R.id.edtAddress);
+        final MaterialEditText edtComment = (MaterialEditText)order_address_comment.findViewById(R.id.edtComment);
+
+        alertDialog.setView(order_address_comment);
         alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
 
         alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
@@ -94,6 +99,8 @@ public class Cart extends AppCompatActivity {
                         Common.currentUser.getName(),
                         edtAddress.getText().toString(),
                         txtTotalPrice.getText().toString(),
+                        "0",
+                        edtComment.getText().toString(),
                         cart
                 );
 
--- app/src/main/java/app/mangoofood/mangooapp/Model/Request.java
+++ app/src/main/java/app/mangoofood/mangooapp/Model/Request.java
@@ -9,30 +9,24 @@ public class Request {
     private String address;
     private String total;
     private String status;
+    private String comment;
     private List<Order> foods;
 
     public Request() {
     }
 
-    public Request(String phone, String name, String address, String total, List<Order> foods) {
+    public Request(String phone, String name, String address, String total, String status, String comment, List<Order> foods) {
         this.phone = phone;
         this.name = name;
         this.address = address;
         this.total = total;
+        this.status = status;
+        this.comment = comment;
         this.foods = foods;
-        this.status = "0";
     }
 
     public String getPhone() {
         return phone;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     public void setPhone(String phone) {
@@ -61,6 +55,22 @@ public class Request {
 
     public void setTotal(String total) {
         this.total = total;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
     }
 
     public List<Order> getFoods() {
